010d06311167302748bf16a213ac97a6cf5a3e4a
Rob397 <105466612+Rob397@users.noreply.github.com>
2022-09-01 17:15:41 +1000

    Update logic.js

--- logic.js
+++ logic.js
@@ -1,4 +1,17 @@
-//example starter code for the map
+
+//Visit the USGS GeoJSON Feed page and pick a data set to visualise. When you click on a data set, 
+//for example 'All Earthquakes from the Past 7 Days', you will be given a JSON representation of that data.
+// You will be using the URL of this JSON to pull in the data for our visualisation.
+
+
+// Create a map using Leaflet that plots all of the earthquakes from your data set based on their longitude and latitude.
+
+// Your data markers should reflect the magnitude of the earthquake in their size and colour. Earthquakes with higher magnitudes should appear larger and darker in colour.
+
+// Include popups that provide additional information about the earthquake when a marker is clicked.
+
+// Create a legend that will provide context for your map data
+
 
 var myMap = L.map("map", {
     center: [40.7128, -74.0059],
@@ -14,7 +27,8 @@ var myMap = L.map("map", {
     accessToken: API_KEY
   }).addTo(myMap);
   
-  var url = "static/data/australia-healthsites.geojson";
+  // 2.5 level earthquake in the last week json url
+  var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojson";
   
   d3.json(url).then(function(response) {
   
