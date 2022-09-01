1ea5826b91ab7aaa4da8d18001a2a53ef579e77c
Rob397 <105466612+Rob397@users.noreply.github.com>
2022-09-01 20:07:08 +1000

    Update logic.js

--- logic.js
+++ logic.js
@@ -20,7 +20,7 @@
 
 
 
-  d3.json(queryUrl).then(function(data) {
+  d3.json(url).then(function(data) {
     // Once we get a response, send the data.features object to the createFeatures function
     createFeatures(data.features);
   });
