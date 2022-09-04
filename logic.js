d60eeb792a1ed6f6212af854efbb8d47730ccfea
Rob397 <105466612+Rob397@users.noreply.github.com>
2022-09-04 19:31:52 +1000

    Update logic.js
    changed the code a bit

--- logic.js
+++ logic.js
@@ -1,132 +1,226 @@
 
-//Visit the USGS GeoJSON Feed page and pick a data set to visualise. When you click on a data set, 
-//for example 'All Earthquakes from the Past 7 Days', you will be given a JSON representation of that data.
-// You will be using the URL of this JSON to pull in the data for our visualisation.
 
 
-// Create a map using Leaflet that plots all of the earthquakes from your data set based on their longitude and latitude.
+// Create a map using Leaflet that plots all of the earthquakes from your data set based on their longitude and latitude..
 
-// Your data markers should reflect the magnitude of the earthquake in their size and colour. Earthquakes with higher magnitudes should appear larger and darker in colour.
+// Include popups that provide additional information about the earthquake when a marker is clicked.Will do later after plots are complete
 
-// Include popups that provide additional information about the earthquake when a marker is clicked.
+//--------------------// Earthquakes with higher magnitudes should appear larger and darker in colour.-------------
 
-// Create a legend that will provide context for your map data
+// colour codes for js
+// Yellow #FFFF00
+// dark orange #ff8c00
+// Red 	#FF0000 
+    // // Function to Determine Color of Marker Based on the Magnitude of the Earthquake
+    // function chooseColor(magnitude) {
+    //   switch (true) {
+    //   case magnitude > 5:
+    //       return "#FF0000 ";
+    //   case magnitude > 3:
+    //       return "#ff8c00";
+    //   case magnitude > 1:
+    //       return "#FFFF00";
+    //   default:
+    //       return "#DAF7A6";
+    //   }
 
+// ----------------------------------------------------------------------------------------------------------------
 
+// 2.5 level earthquake in the last week json url
 
+  // feature.geometry.coordinates[2] is the data I want for long and lat
+
+  // d3.json(url).then(function (data) {
+  //   // Once we get a response, send the data.features object to the createFeatures function
+  //   createFeatures(data.features);
+  // }).addto(myMap);
+
+  // d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function (data) {
+//  This function returns the style data for each of the earthquakes we plot on  
+//  the map. We pass the magnitude of the earthquake into two separate functions  
+//  to calculate the color and radius. 
+
+//  function styleInfo(feature) {    
+//   return {      opacity: 1,      
+//   fillOpacity: 1,      fillColor: getColor(feature.geometry.coordinates[2]),      
+//   color: "#000000",      
+//   radius: getRadius(feature.properties.mag),      
+//   stroke: true,      
+//   weight: 0.5    };  
+// };
+// })
   
-  // 2.5 level earthquake in the last week json url
-  var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojson";
 
 
 
-  d3.json(url).then(function(data) {
-    // Once we get a response, send the data.features object to the createFeatures function
-    createFeatures(data.features);
-  });
-  
-    // Create a GeoJSON layer containing the features array on the earthquakeData object
-    // Run the onEachFeature function once for each piece of data in the array
-    var earthquakes = L.geoJSON(earthquakeData, {
-      onEachFeature: onEachFeature
-    });
-    var myMap = L.map("map", {
-      center: [40.7128, -74.0059],
-      zoom: 2
-    });
 
-    function createFeatures(earthquakeData) {
-  
-      // Define a function we want to run once for each feature in the features array
-      // Give each feature a popup describing the place and time of the earthquake
-      function onEachFeature(feature, layer) {
-        layer.bindPopup("<h3>" + feature.properties.place +
-          "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
-      }
-    
-    
-    L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-      attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-      tileSize: 512,
-      maxZoom: 18,
-      zoomOffset: -1,
-      id: "mapbox/streets-v11",
-      accessToken: API_KEY
-    }).addTo(myMap);  
-  //   // Sending our earthquakes layer to the createMap function
-  //   createMap(earthquakes);
-  // }function createMap(earthquakes) {
+var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
-  //   // Define streetmap and darkmap layers
-  //   var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-  //     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-  //     tileSize: 512,
-  //     maxZoom: 18,
-  //     zoomOffset: -1,
-  //     id: "mapbox/streets-v11",
-  //     accessToken: API_KEY
-  //   });
+
+// Initialize all of the LayerGroups we'll be using
+
+// Create the map with our layers
+var map = L.map("map", {
+  center: [-33.4500, -70.667],
+  zoom: 10,
+
+});
+
+// Add a tile layer
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/streets-v11",
+  accessToken: API_KEY
+}).addTo(map);
+
+// Grabbing our GeoJSON data..
+d3.json(url).function(data) {
+  // Creating a GeoJSON layer with the retrieved data
+  console.log(data);
+};
+
+
+
+
+
+
+
+// // Loop through the cities array and create one marker for each city object
+// for (var i = 0; i < data.length; i++) {
+
+//   // Conditionals for countries points
+//   var color = "";
+//   if (data[i].points > 200) {
+//     color = "yellow";
+//   }
+//   else if (countries[i].points > 100) {
+//     color = "blue";
+//   }
+//   else if (countries[i].points > 90) {
+//     color = "green";
+//   }
+//   else {
+//     color = "red";
+//   }
+
+//   // Add circles to map
+//   L.circle(countries[i].location, {
+//     fillOpacity: 0.75,
+//     color: "white",
+//     fillColor: color,
+//     // Adjust radius
+//     radius: countries[i].points * 1500
+//   }).bindPopup("<h1>" + countries[i].name + "</h1> <hr> <h3>Points: " + countries[i].points + "</h3>").addTo(myMap);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// cities in chili
+// Santiago	‑33.4500	‑70.6667, 7007000
+// Concepción	‑36.8200	‑73.0445, 889725
+// Puente Alto	‑33.6167	‑70.5833, 573935
+
+
+// // An array containing each city's name, location, and population // example for popup loop
+// var cities = [{
+//   location: [-33.4500, -70.667],
+//   name: "Santiago",
+//   population: 7007000
+// },
+// {
+//   location: [-36.8200, -73.0445],
+//   name: "Concepción",
+//   population: 889725
+// },
+// {
+//   location: [-33.6167, -70.5833],
+//   name: "Puente Alto",
+//   population: 573935
+// },
+
+// ];
+
+
+// // example loop for popup
+// for (var i = 0; i < cities.length; i++) {
+//   var city = cities[i];
+//   L.marker(city.location)
+//     .bindPopup("<h1>" + city.name + "</h1> <hr> <h3>Population " + city.population + "</h3>")
+//     .addTo(myMap);
+// }
+
+// example circle
+// var circle = L.circle([-33.4500, -70.667], {
+//   color: "green",
+//   fillColor: "#f03",
+//   fillOpacity: 0.8,
+//   radius: 500.0
+// }).addTo(map);
+
+
+// Your data markers should reflect the magnitude of the earthquake in their size and colour. 
+// Earthquakes with higher magnitudes should appear larger and darker in colour.
+// USe the logic for conditional looping to do above
+
+
+
+
+
+
+
+
+// // change variable names
+// for (var i = 0; i < cities.length; i++) {
+
+//   // Create a new station object with properties of both station objects
+//   var city = Object.assign({}, cities[i], population[i]);
+//   // If a city has a different population size then change the color of the 
+//   // circle
   
-  //   var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-  //     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-  //     maxZoom: 18,
-  //     id: "dark-v10",
-  //     accessToken: API_KEY
-  //   });
-  
-  //   // Define a baseMaps object to hold our base layers
-  //   var baseMaps = {
-  //     "Street Map": streetmap,
-  //     "Dark Map": darkmap
-  //   };
-  
-  //   // Create overlay object to hold our overlay layer
-  //   var overlayMaps = {
-  //     Earthquakes: earthquakes
-  //   };
-  
-  //   // Create our map, giving it the streetmap and earthquakes layers to display on load
-  //   var myMap = L.map("map", {
-  //     center: [
-  //       37.09, -95.71
-  //     ],
-  //     zoom: 5,
-  //     layers: [streetmap, earthquakes]
-  //   });
-  
-  //   // Create a layer control
-  //   // Pass in our baseMaps and overlayMaps
-  //   // Add the layer control to the map
-  //   L.control.layers(baseMaps, overlayMaps, {
-  //     collapsed: false
-  //   }).addTo(myMap);
-  }
-  
-  
-    // Grab the data with d3
-    // d3.json(url).then(function (response) {
+//   if (cities.population < 890000) {
+   
     
-    //     console.log(response)
-    //     // Create a new marker cluster group
-    //     var markers = L.markerClusterGroup();
-    
-    //     // Loop through data
-    //     for (var i = 0; i < response.length; i++) {
-    
-    //         // Set the data location property to a variable
-    //         var location = response[i].location;
-    
-    //         // Check for location property
-    //         if (location) {
-    
-    //             // Add a new marker to the cluster group and bind a pop-up
-    //             markers.addLayer(L.marker([location.coordinates[1], location.coordinates[0]])
-    //                 .bindPopup(response[i].descriptor));
-    //         }
-    
-    //     }
-    
-    //     // Add our marker cluster layer to the map
-    //     myMap.addLayer(markers);
-    
-    // });
+     
+//     };
+//   }
+
+
+//   // If a station has no bikes available, it's empty
+//   else if (!station.num_bikes_available) {
+//     stationStatusCode = "EMPTY";
+//   }
+//   // If a station is installed but isn't renting, it's out of order
+//   else if (station.is_installed && !station.is_renting) {
+//     stationStatusCode = "OUT_OF_ORDER";
+//   }
+//   // If a station has less than 5 bikes, it's status is low
+//   else if (station.num_bikes_available < 5) {
+//     stationStatusCode = "LOW";
+//   }
+//   // Otherwise the station is normal
+//   else {
+//     stationStatusCode = "NORMAL";
+//   }};
